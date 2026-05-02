552525d2c400ecdcc5bfc1cdf1dfd9a5d53022ae
Yashas <yashashd1805@gmail.com>
2026-05-02 16:22:20 +0530

    fix cors preflight crash

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
-app.options('*', cors());
+app.options("/*", cors());
 app.use(express.json());
 
 // Routes
